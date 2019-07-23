020d84a51bcc4878c3f02ef926860df50e5af57c
martinsodabi <martinsdennis86@gmail.com>
2019-07-24 08:18:22 +0900

    Placed Site in Progress Text

--- svelte-frontend/src/App.svelte
+++ svelte-frontend/src/App.svelte
@@ -1,20 +1,26 @@
 <script>
   import Account from "./Account.svelte";
-  import ApplloClient from "apollo-boost";
-  import { setClient } from "svelte-apollo";
-  const client = new ApplloClient({
-    uri: "https://swapboard.herokuapp.com/graphql"
-  });
-  setClient(client);
+  // import ApplloClient from "apollo-boost";
+  // import { setClient } from "svelte-apollo";
+  // const client = new ApplloClient({
+  //   uri: "https://swapboard.herokuapp.com/graphql"
+  // });
+  // setClient(client);
 </script>
 
 <style>
   h1 {
-    color: purple;
+    color: rgb(63, 63, 63);
+    text-align: center;
+  }
+  h3 {
+    color: rgb(63, 63, 63);
+    text-align: center;
   }
 </style>
 
 <main>
-  <h1>SWAP BOARD</h1>
-  <Account />
+  <h1>SWAP BOARD DEVELOPMENT IN PROGRESS</h1>
+  <h3>Check back later</h3>
+  <!-- <Account /> -->
 </main>
